0a20c6300f8720b887259b4316a472421e08f62c
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-07-28 19:59:19 +0200

    setup store redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import "./global.css";
 import { App } from "./App";
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
 );
